5216c8a276142ccf67178dbf106b51dd64bbff83
Andrii <andriy30534>
2024-03-16 23:47:43 +0200

    added store

---
+++ src/store/userStote.ts
@@ -0,0 +1,79 @@
+import {create} from 'zustand';
+import {IUserType} from '../types/storeTypes';
+import {getUser, loginUser, logoutUser, registerUser} from '../api';
+import {AxiosError} from 'axios';
+import SecureToken from '../helper/Secure';
+
+export const useUserStore = create<IUserType>()(set => ({
+  user: null,
+  error: null,
+  loading: false,
+  isUser: false,
+  registerUser: async body => {
+    set({loading: true});
+    try {
+      const data = await registerUser(body);
+      SecureToken.storeToken(data.token);
+
+      set({
+        user: data.user,
+        isUser: true,
+        loading: false,
+      });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        set({loading: false, error: error});
+      }
+    }
+  },
+  loginUser: async body => {
+    set({loading: true});
+    try {
+      const data = await loginUser(body);
+
+      SecureToken.storeToken(data.token);
+
+      set({
+        user: data.user,
+        isUser: true,
+        loading: false,
+      });
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        set({loading: false, error: error});
+      }
+    }
+  },
+  getUser: async () => {
+    set({loading: true});
+    try {
+      const user = await getUser();
+      set({
+        user,
+        isUser: true,
+        loading: false,
+      });
+    } catch (error) {
+      set({loading: false});
+      if (error instanceof AxiosError) {
+        set({loading: false, error: error});
+      }
+    }
+  },
+  logoutUser: async () => {
+    set({loading: true, user: null});
+    try {
+      await logoutUser();
+
+      set({
+        isUser: false,
+        loading: false,
+      });
+      SecureToken.removeToken();
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        set({loading: false, error: error});
+      }
+    }
+  },
+}));
